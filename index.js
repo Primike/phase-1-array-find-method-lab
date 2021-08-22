0ee4741f639ca313e6913c9e0b4491c3ff20cb97
Prince <avecillasprince@yahoo.com>
2021-08-22 13:25:11 -0400

    commit tree 4f96230d4dcc1bc201114da837cdb826a0a399c8

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-superbowlWin = (record) => {
-    const result = record.find( record => record.result === "W" );
-    return !!result ? result.year : undefined;
+function superbowlWin(x) {
+  let y = x.find(x => x.result === "W")
+  if (y) {
+    return y.year
   }
+  else{
+    return undefined
+  }
+}
+
+
+
+/* superbowlWin = (record) => {
+  const result = record.find( record => record.result === "W" );
+  return !!result ? result.year : undefined;
+}
+ */
